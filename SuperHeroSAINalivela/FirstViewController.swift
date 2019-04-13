d52d27dd0a4ea41082731bd0903d99b62d842d82
Student <student@nw46147.local>
2019-04-13 17:56:48 -0500

    Initial Commit

---
+++ SuperHeroSAINalivela/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SuperHeroSAINalivela
+//
+//  Created by Student on 4/13/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SuperHeroSAINalivela/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SuperHeroSAINalivela
+//
+//  Created by Student on 4/13/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
